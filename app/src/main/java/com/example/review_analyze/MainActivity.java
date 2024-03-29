79c267acbd42f91184c3982eda5666f3e4011389
salve <bbuullaatt666@gmail.com>
2024-03-29 15:27:26 +0500

    1

---
+++ app/src/main/java/com/example/review_analyze/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.review_analyze;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
